01707ce1dd9730ac06c46cf4a675f3fa4c79bdb6
Manikandan Shanmugam <e0284006@u.nus.edu>
2018-02-25 20:45:25 +0800

    poly

--- marks/Program.cs
+++ marks/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,40 @@ namespace marks
     {
         static void Main(string[] args)
         {
+            
+
+            StudentMark m1 = new StudentMark("xyz", "0006", 78, 89, 92);
+            StudentMark m2 = new StudentMark("qrt", "0007", 87, 98, 77);
+            StudentMark m3 = new StudentMark("pok", "0008", 99, 94, 95);
+            S2 m4 = new S2("cgr", "0009", 95, 79, 90);
+            Drawing d = new Drawing();
+            d.Add(m1);
+            d.Add(m2);
+            d.Add(m3);
+            d.Add(m4);
+            d.Draw();
+
+
+        }
+    }
+
+    class Drawing
+    {
+        ArrayList allshapes = new ArrayList();
+        public void Add(Sa46 s)
+        {
+            allshapes.Add(s);
+        }
+        public void Draw()
+        {
+            for (int i = 0; i < allshapes.Count; i++)
+            {
+                Sa46 s = (Sa46)  allshapes[i];
+                s.ToatalCal();
+                s.AvgMark();
+                Console.WriteLine(s.Show());
+                Console.WriteLine();
+            }
         }
     }
 }
---
+++ marks/sa46.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace marks
+{
+    class Sa46
+    {
+        string name;
+        string rollnumber;
+        double m1;
+        double m2;
+        double m3;
+        double total;
+        double avg;
+
+        public Sa46()
+        {
+
+        }
+
+        public string Name
+        {
+            set
+            {
+                name = value;
+            }
+            get
+            {
+                return name;
+            }
+
+        }
+
+        public string Roll
+        {
+            set
+            {
+                rollnumber = value;
+            }
+            get
+            {
+                return rollnumber;
+            }
+        }
+
+        public double M1
+        {
+            set
+            {
+                m1 = value;
+            }
+            get
+            {
+                return m1;
+            }
+        }
+        public double M2
+        {
+            set
+            {
+                m2 = value;
+            }
+            get
+            {
+                return m2;
+            }
+        }
+        public double M3
+        {
+            set
+            {
+                m3 = value;
+            }
+            get
+            {
+                return m3;
+            }
+        }
+
+        public virtual double ToatalCal()
+        {
+            total = m1 + m2 + m3;
+            return total;
+
+        }
+
+        public virtual double AvgMark()
+        {
+            avg = total / 3;
+            return avg;
+        }
+
+        public virtual string Show()
+        {
+            string s = string.Format("NAME={0}\nTOTALMARK={1}\nAVERAGE={2:0.00}", Name, total, avg);
+            return s;
+        }
+    }
+        class StudentMark : Sa46
+        {
+
+           public StudentMark(string name,string roll,double m1,double m2,double m3) : base()
+        {
+            base.Name = name;
+            base.Roll = roll;
+            base.M1 = m1;
+            base.M2 = m2;
+            base.M3 = m3;
+        }
+
+        public override double AvgMark()
+        {
+            return base.AvgMark();
+        }
+
+        public override double ToatalCal()
+        {
+            return base.ToatalCal();
+        }
+
+        public override string Show()
+        {
+            return base.Show();
+        }
+    }
+
+    class S2 : Sa46
+    {
+        public S2(string name, string roll, double m1, double m2, double m3) : base()
+        {
+            base.Name = name;
+            base.Roll = roll;
+            base.M1 = m1;
+            base.M2 = m2;
+            base.M3 = m3;
+        }
+
+        public override double AvgMark()
+        {
+            return base.AvgMark();
+        }
+
+        public override double ToatalCal()
+        {
+            return base.ToatalCal();
+        }
+
+        public override string Show()
+        {
+            return base.Show();
+        }
+    }
+    
+}
